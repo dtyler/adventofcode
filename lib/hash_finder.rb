e3236119e5415b0f5dd2f579c7be9f96f095b2f4
Dan Tyler <dtyler@constantcontact.com>
2015-12-15 08:26:28 -0500

    Day 4 part 2 : Faster looping

--- lib/hash_finder.rb
+++ lib/hash_finder.rb
@@ -1,17 +1,16 @@
 require 'digest'
 
 class HashFinder
-  def initialize(secret_key)
+  def initialize(secret_key, front_padding_count)
     @secret_key = secret_key
+    @front_padding_count = front_padding_count
+    @Inf = 1.0 / 0.0
   end
 
   def compute
-    candidate = 0
-    hash = Digest::MD5.hexdigest("#{@secret_key}#{candidate}")
-    until hash[0..4].eql? '00000'
-      candidate += 1
-      hash = Digest::MD5.hexdigest("#{@secret_key}#{candidate}")
+    padding = '0' * @front_padding_count
+    (1..@Inf).find do |suffix|
+      Digest::MD5.hexdigest(@secret_key + suffix.to_s).start_with?(padding)
     end
-    candidate
   end
 end
--- spec/hash_finder_spec.rb
+++ spec/hash_finder_spec.rb
@@ -2,7 +2,7 @@ require 'hash_finder'
 
 RSpec.describe HashFinder do
   it 'finds decimal pad for secret key' do
-    expect(HashFinder.new('abcdef').compute).to eq 609_043
-    expect(HashFinder.new('pqrstuv').compute).to eq 1_048_970
+    expect(HashFinder.new('abcdef', 5).compute).to eq 609_043
+    expect(HashFinder.new('pqrstuv', 5).compute).to eq 1_048_970
   end
 end
