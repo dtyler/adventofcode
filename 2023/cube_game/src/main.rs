f5f2b53a137786c6eaa2e16e7012740b3834f28a
Daniel Tyler <dtyler@csh.rit.edu>
2023-12-08 19:08:44 -0500

    Game parsing appears to work

--- 2023/cube_game/src/main.rs
+++ 2023/cube_game/src/main.rs
@@ -2,13 +2,15 @@ use std::{fs, str::FromStr};
 
 #[derive(Debug)]
 struct GameIteration {
-    num_blue: usize,
-    num_red: usize,
-    num_green: usize,
+    num_blue: i32,
+    num_red: i32,
+    num_green: i32,
 }
 
 #[derive(Debug, PartialEq, Eq)]
-struct ParseGameIterationError;
+struct ParseGameIterationError {
+    msg: String,
+}
 
 #[derive(Debug)]
 struct CubeGame {
@@ -20,8 +22,39 @@ impl FromStr for GameIteration {
     type Err = ParseGameIterationError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        println!("DEBUG {}", s);
-        todo!()
+        // println!("DEBUG '{}'", s);
+        if s.trim().is_empty() {
+            return Err(ParseGameIterationError {
+                msg: String::from("Given empty string"),
+            });
+        }
+        let mut new_game_iter = GameIteration {
+            num_blue: 0,
+            num_green: 0,
+            num_red: 0,
+        };
+        for iter in s.split(',') {
+            let cleaned = iter.trim();
+            let sep_index = cleaned
+                .find(' ')
+                .expect("every game iteration must be of the form '<num> <color>' ");
+            let number = cleaned[..sep_index].parse::<i32>().expect("parse failed");
+            let color = &cleaned[(sep_index + 1)..];
+            match color {
+                "blue" => new_game_iter.num_blue = number,
+                "red" => new_game_iter.num_red = number,
+                "green" => new_game_iter.num_red = number,
+                _ => {
+                    return Err(ParseGameIterationError {
+                        msg: format!(
+                            "unexpected color '{}' given. sep_index was {}",
+                            color, sep_index
+                        ),
+                    })
+                }
+            }
+        }
+        Ok(new_game_iter)
     }
 }
 
@@ -39,15 +72,20 @@ fn parse_line(line: &str) -> CubeGame {
         .expect("Couldn't get game id")
         .parse::<i32>()
         .expect("game id could not be parsed as number");
-    let mut cursor = first_semicolon;
+    let mut cursor = first_semicolon + 1;
     let mut games: Vec<GameIteration> = vec![];
-    while cursor < line.len() {
+    loop {
         let rest_of_line = &line[cursor..];
         if let Some(next_semicolon) = rest_of_line.find(';') {
             let game_slice = &rest_of_line[..next_semicolon];
-            println!("rest_of_line: {:?}\npre parse {}", rest_of_line, game_slice);
-            games.push(game_slice.parse::<GameIteration>().unwrap());
-            cursor = next_semicolon;
+            // println!("rest_of_line: {:?}\npre parse {}", rest_of_line, game_slice);
+            match game_slice.parse::<GameIteration>() {
+                Ok(game) => games.push(game),
+                Err(error) => panic!("{}", error.msg),
+            }
+            cursor += next_semicolon + 1;
+        } else {
+            break;
         }
     }
     CubeGame { game_id, games }
